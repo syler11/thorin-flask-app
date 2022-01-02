6500523dcd36dc938adbe7c15698b834bc3428e6
syler11 <nemeth.szilard82@gmail.com>
2022-01-02 08:24:43 +0000

    compnay.json added

--- run.py
+++ run.py
@@ -11,7 +11,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    data= []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
