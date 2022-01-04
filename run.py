5666a3f1c13dcd3e4526557ab1988b9ef1f9867b
syler11 <nemeth.szilard82@gmail.com>
2022-01-04 14:48:18 +0000

    contact.html contact form added

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html" member=member)
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
