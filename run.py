8de5b896f58edea104d08b58cc015e3c052f3793
syler11 <nemeth.szilard82@gmail.com>
2022-01-05 07:05:07 +0000

    user message displayed after sending form

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import os
 import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
-    import env.py
+    import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def index():
@@ -14,7 +15,7 @@ def index():
 
 @app.route("/about")
 def about():
-    data= []
+    data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
@@ -33,7 +34,7 @@ def about_member(member_name):
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method =="POST":
+    if request.method == "POST":
         flash("Thanks {}, we have received your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
